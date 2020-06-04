262cf68df60f8265a99529d52b62d20c7ea49106
stockypro <66403026+stockypro@users.noreply.github.com>
2020-06-04 11:21:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const cron = require("node-cron");
 const option_chain = require('./nse_lib');
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,9 +7,6 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
 
-cron.schedule('0 0,30 * * * *', function() {
-	console.log("schedule is running...");
-});
 
 app.get('/', (req, res) => res.redirect('/index.html'));
 app.get('/chain', async (req, res) => {
