b643010cb4fede2abc38fa69bec97b0e8dc93d23
stockypro <66403026+stockypro@users.noreply.github.com>
2020-06-04 11:10:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const fs = require('fs');
+const cron = require("node-cron");
 const option_chain = require('./nse_lib');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-app.use('ready',() => {
-	console.log('Ready!');
-	var CronJob = require('cron').CronJob;
-	var remind = new CronJob('0 0,30 * * * *', function() {
-	  console.log('check to see if dyno is on');
-	}, 
-	remind.start();
-});
-    
+
 app.use(express.static('public'))
+
+cron.schedule('0 0,20 * * * *', function() {
+	console.log("schedule is running...");
+});
 
 app.get('/', (req, res) => res.redirect('/index.html'));
 app.get('/chain', async (req, res) => {
